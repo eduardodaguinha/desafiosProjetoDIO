5c84c2ff8c62f258d114da03ee2bd2a548e29a85
eduardodaguinha <eduardosousadaga@icloud.com>
2024-09-10 23:19:15 -0300

    1ª Correção de Indentação do Desafio de Projeto 6

--- desafioProjeto6/script.js
+++ desafioProjeto6/script.js
@@ -1,38 +1,40 @@
 // Classe para representar um herói de aventura
 class Heroi {
-    // Construtor para inicializar as propriedades do herói
-    constructor(nome, idade, tipo) {
-        this.nome = nome; // Nome do herói
-        this.idade = idade; // Idade do herói
-        this.tipo = tipo; // Tipo do herói (guerreiro, mago, monge, ninja)
+  // Construtor para inicializar as propriedades do herói
+  constructor(nome, idade, tipo) {
+    this.nome = nome; // Nome do herói
+    this.idade = idade; // Idade do herói
+    this.tipo = tipo; // Tipo do herói (guerreiro, mago, monge, ninja)
+  }
+
+  // Método para o herói atacar com base no seu tipo
+  atacar() {
+    let ataque;
+
+    // Determinando o tipo de ataque com base no tipo do herói
+    switch (this.tipo.toLowerCase()) {
+      case "mago":
+        ataque = "usou magia."; // Ataque para o mago
+        break;
+      case "guerreiro":
+        ataque = "utilizou sua espada."; // Ataque para o guerreiro
+        break;
+      case "monge":
+        ataque = "praticou artes marciais."; // Ataque para o monge
+        break;
+      case "ninja":
+        ataque = "lançou uma shuriken."; // Ataque para o ninja
+        break;
+      default:
+        ataque = "realizou um ataque desconhecido."; // Caso o tipo seja desconhecido
+        break;
     }
 
-    // Método para o herói atacar com base no seu tipo
-    atacar() {
-        let ataque;
-
-        // Determinando o tipo de ataque com base no tipo do herói
-        switch (this.tipo.toLowerCase()) {
-            case "mago":
-                ataque = "usou magia."; // Ataque para o mago
-                break;
-            case "guerreiro":
-                ataque = "utilizou sua espada."; // Ataque para o guerreiro
-                break;
-            case "monge":
-                ataque = "praticou artes marciais."; // Ataque para o monge
-                break;
-            case "ninja":
-                ataque = "lançou uma shuriken."; // Ataque para o ninja
-                break;
-            default:
-                ataque = "realizou um ataque desconhecido."; // Caso o tipo seja desconhecido
-                break;
-        }
-
-        // Exibindo a mensagem de ataque do herói
-        console.log(`${this.tipo.charAt(0).toUpperCase() + this.tipo.slice(1)} ${ataque}`);
-    }
+    // Exibindo a mensagem de ataque do herói
+    console.log(
+      `${this.tipo.charAt(0).toUpperCase() + this.tipo.slice(1)} ${ataque}`
+    );
+  }
 }
 
 // Criando instâncias dos heróis com diferentes tipos
