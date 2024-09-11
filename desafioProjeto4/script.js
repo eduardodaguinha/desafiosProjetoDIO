8f421b93f9502d4446bb5d386aeb87443c2ab0ec
eduardodaguinha <eduardosousadaga@icloud.com>
2024-09-10 23:18:42 -0300

    1ª Correção de Indentação do Desafio de Projeto 4

--- desafioProjeto4/script.js
+++ desafioProjeto4/script.js
@@ -1,25 +1,25 @@
 // Declaração das variáveis
 let nomeHeroi = "Homem de Ferro";
-let xpHeroi = 9500;       
+let xpHeroi = 9500;
 let nivelHeroi = "";
 
 // Estrutura de decisão para determinar o nível do herói
 if (xpHeroi < 1000) {
-    nivelHeroi = "Ferro";
+  nivelHeroi = "Ferro";
 } else if (xpHeroi <= 2000) {
-    nivelHeroi = "Bronze";
+  nivelHeroi = "Bronze";
 } else if (xpHeroi <= 5000) {
-    nivelHeroi = "Prata";
+  nivelHeroi = "Prata";
 } else if (xpHeroi <= 7000) {
-    nivelHeroi = "Ouro";
+  nivelHeroi = "Ouro";
 } else if (xpHeroi <= 8000) {
-    nivelHeroi = "Platina";
+  nivelHeroi = "Platina";
 } else if (xpHeroi <= 9000) {
-    nivelHeroi = "Ascendente";
+  nivelHeroi = "Ascendente";
 } else if (xpHeroi <= 10000) {
-    nivelHeroi = "Imortal";
+  nivelHeroi = "Imortal";
 } else {
-    nivelHeroi = "Radiante";
+  nivelHeroi = "Radiante";
 }
 
 // Saída da mensagem com o nível do herói
